9c6a1b71e86d2bfc39f45646a2d6c9e1a894c5d5
juani <juani.trotti@gmail.com>
2022-04-13 19:51:13 -0300

    desafios

---
+++ desafios/clases-Clase2/desafioClases.js
@@ -0,0 +1,20 @@
+class user {
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = [libros];
+        this.mascotas = [mascotas];
+    }
+
+    getFullName = () => {}
+
+    addMascota = () => {}
+
+    countMasctotas = () => {}
+
+    addBook = () => {}
+
+    gerBookName = () => {}
+}
+
+console.log("hola");
