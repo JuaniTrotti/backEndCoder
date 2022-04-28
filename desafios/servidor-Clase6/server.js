68070ce5c8910cd392865e4bdc77784e392b9193
juani <juani.trotti@gmail.com>
2022-04-27 22:17:10 -0300

    serverExpressResuelto

--- desafios/servidor-Clase6/server.js
+++ desafios/servidor-Clase6/server.js
@@ -1,55 +1,40 @@
+const express = require('express');
 const contenedor = require('./Contenedor.js')
 
-const productosTienda = [
-    {title: "Laptop", price: 20000, thumbnail: "laptop.jpg"},
-    {title: "Television", price: 10000, thumbnail: "tv.jpg"},
-    {title: "Tablet", price: 5000, thumbnail: "tablet.jpg"},
-];
+
 const tienda = new contenedor('./prueba.txt');
 
-async function cargarProductos(productos) {
-    for (let i of productos) {
-        await tienda.save(i);
-    }
+const PORT = 8080;
+const app = express();
+
+async function idRandom() {
+    const arr = await tienda.getAll();
+    const num = Math.floor((Math.random() * (arr.length-1))+1);
+    const elemento = await tienda.getById(num);
+    return elemento
 }
 
-async function getById(id) {
-    let res = await tienda.getById(id);
-    console.log(res);
+async function all() {
+    let datos = await tienda.getAll();
+    return datos;
 }
 
-async function deleteById(id) {
-    let res = await tienda.deleteById(id);
-    console.log(res);
-}
+const server = app.listen(PORT, () => {
+    console.log('servidor escuchando en el puerto ' + PORT);
+})
 
-async function deleteAll() {
-    await tienda.deleteAll();
-}
 
-async function getAll() {
-    let res = await tienda.getAll();
-    console.log(res);
-}
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la tienda de productos');
+})
 
-cargarProductos(productosTienda);
 
-setTimeout(() => {
-    getAll();
-}, 1000);
+app.get('/productoRandom', async function (req, res) {
+    const data = await idRandom();
+    res.send({data});
+})
 
-setTimeout(() => {
-    getById(2);
-}, 2000);
-
-setTimeout(() => {
-    deleteById(2);
-}, 3000);
-
-setTimeout(() => {
-    getAll();
-}, 4000);
-
-// setTimeout(() => {
-//     deleteAll();
-// }, 5000);
+app.get('/productos', async function (req, res) {
+    const data = await all();
+    res.send({data});
+})
